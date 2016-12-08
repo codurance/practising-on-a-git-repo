2f2c7709e055fed60bc2909f325a98e6ce58dcd5
Mani Sarkar <mani@codurance.com>
2016-12-08 10:58:25 +0000

    Added makeBranchesAccessible.sh to this branch - related to some pesky git-bash related issue

---
+++ makeBranchesAccessible.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e 
+
+listOfBranches=$(git branch --all | grep -v "/HEAD" | grep -v "/master" | tr '/' ' ' | awk '{print $3}');
+listOfBranches=(`echo ${listOfBranches}`);
+
+echo "Checkout out master"
+git checkout master
+
+for branchName in "${listOfBranches[@]}"
+do
+  echo "Checkout out $branchName..."
+  git checkout $branchName
+done
+
+echo "Checkout out master"
+git checkout master
+
+echo "List of all branches that can now be accessed by git commands"
+git branch --list
